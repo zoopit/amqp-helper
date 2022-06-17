b1d600aed5ff481bf389e445da6503637447dc4e
babak-asadi <babak@zoopit.no>
2022-06-17 11:12:25 +0200

    chore(ts check): TS compile added to package.json

--- src/ts-support.d.ts
+++ src/ts-support.d.ts
@@ -1,7 +1,7 @@
-import { Connection } from "amqplib" 
+import { Connection as Connection1 } from "amqplib" 
 
-export function connect(uri: string): Promise<Connection>;
+export function connect(uri: string): Promise<Connection1>;
 
-export function sendWithResponseQueue<T,R>(connection: Connection , queueOut: string , queueIn: string , body: T): Promise<R>;
+export function sendWithResponseQueue<T,R>(connection: Connection1 , queueOut: string , queueIn: string , body: T): Promise<R>;
 
-export type Connection = Connection
+export type Connection = Connection1
