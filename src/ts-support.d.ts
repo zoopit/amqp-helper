af8973ec8e9ca26708d60f84eb693f92af2af7bb
babak-asadi <babak@zoopit.no>
2022-06-20 15:15:06 +0200

    chore(comments): some comments added

--- src/ts-support.d.ts
+++ src/ts-support.d.ts
@@ -1,5 +1,8 @@
 import { Connection as Connection1 } from "amqplib" 
 
+// Just declaration file for ts-support.js
+
+
 export function connect(uri: string): Promise<Connection1>;
 
 export function sendWithResponseQueue<T,R>(connection: Connection1 , queueOut: string , queueIn: string , body: T): Promise<R>;
--- src/ts-support.js
+++ src/ts-support.js
@@ -1,6 +1,7 @@
 const amqp_helper = require('./index')
 const util = require('util')
 
+// Just expoert promisified functions
 module.exports = {
 
     connect: util.promisify(amqp_helper.connect),        
